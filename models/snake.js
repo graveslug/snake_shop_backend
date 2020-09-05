18cabee2a0573a4f06faa2411673b1a244fe5c80
Lillian Parker <Lillianpparker@outlook.com>
2020-09-05 13:43:55 -0500

    Created Snake Schema

---
+++ models/snake.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Sex = ["Male", "Female", "Unknown"]
+
+//Snake Schema
+const SnakeSchema = new Schema({
+        image: { type: String, required: true},
+        species: { type: String, required: true},
+        venomous: { type: Boolean, required: true},
+        morph: { type: String, required: true},
+        sex: { type: String, values: Sex, required: true},
+        prooven: { type: Boolean, required: true},
+        origin: { type: String, required: true},
+        quote: { type: String, required: true},
+        description: { type: String, required: true}
+    })
+
+const SnakeInfo = mongoose.model('SnakeInfo', SnakeSchema)
+
+module.exports = SnakeInfo
